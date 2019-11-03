46d9807e3a20a55c904317f5affd859e7d86da4b
soudk <soud.alkharusi@mail.mcgill.ca>
2019-11-03 09:09:18 -0500

    rotate fix

--- rotateTiles.py
+++ rotateTiles.py
@@ -10,30 +10,32 @@ import imutils
 import glob, os
 import scipy.misc
 
-if os.path.exists('./rot_tiles/') == False:
-    os.makedirs('rot_tiles/')
+if os.path.exists('./rot-tiles/') == False:
+    os.makedirs('rot-tiles/')
 else:
     pass
+for i in range(20):
+    for i,file in enumerate(glob.glob("tiles/*.jpg")):
 
-for i,file in enumerate(glob.glob("tiles/*.jpg")):
-
-    #print(file)
-    split=file.split("_")
-    if ("RA" in file) or ("DEC" in file):
-    	#print(file)
-    	#print(file)
-    	continue
-    #print(split[1])
-    image = cv2.imread(file) 
-    height, width = image.shape[:2]
-    #print(height, width)
-    angle = np.random.randint(1, 360)
-    side_length_factor = np.random.random()*3 + 1
-    #print(angle)
-    rot_image = imutils.rotate(image, angle)
-    rot_image = imutils.resize(rot_image, width = int(width*side_length_factor), height = int(height*side_length_factor))
-    
-    scipy.misc.toimage(rot_image, cmin=0.0, cmax=1.0).save('rot_tiles/' + str(split[1]) + '_'+ 'rot' +'_' +str(angle) + '_' + 'zoom' + '_' + str(side_length_factor) + '.jpg')
+        #print(file)
+        split=file.split("_")
+        if ("RA" in file) or ("DEC" in file):
+        	#print(file)
+        	#print(file)
+        	continue
+        #print(split[1])
+        image = cv2.imread(file) 
+        height, width = image.shape[:2]
+        #print(height, width)
+        angle = np.random.randint(1, 360)
+        side_length_factor = np.random.random()*3 + 1
+        #print(angle)
+        w= int(width*side_length_factor)
+        h=int(height*side_length_factor)
+        rot_image = imutils.rotate(image, angle)
+        #rot_image = imutils.resize(rot_image, width = w, height = h)
+        #rot_image=np.reshape(rot_image, (w, h))
+        scipy.misc.toimage(rot_image, cmin=0.0, cmax=1.0).save('rot-tiles/' + str(split[1]) + '_'+ 'rot' +'_' +str(angle) + '.jpg')
 
 	#cv2.imwrite('rot_tiles/' +str(file) + str(i) + 'rot' + str(angle) + '.jpg', rot_image)
 
