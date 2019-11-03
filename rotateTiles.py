83ea6b4fc9bfb55a59071bf773bfc4467a24af62
soudk <soud.alkharusi@mail.mcgill.ca>
2019-11-03 00:42:14 -0400

    rotate update

--- rotateTiles.py
+++ rotateTiles.py
@@ -7,13 +7,14 @@ Created on Sat Nov  2 21:38:53 2019
 import cv2
 import numpy as np
 import imutils
+import glob, os
 
-for i in len(2880):
-    image = cv2.imread('tile'+str(i)+'.jpg') 
+for i,file in enumerate(glob.glob("tiles/*.jpg")):
+    image = cv2.imread(file) 
     height, width = image.shape[:2]
     print(height, width)
     angle = np.random.randint(1, 360)
     print(angle)
     rot_image = imutils.rotate_bound(image, angle)
-    cv2.imwrite('/rot_tiles/tile'+str(i)+'rot')
+    cv2.imwrite(str(file)+str(i)+'rot.jpg', rot_image)
 
