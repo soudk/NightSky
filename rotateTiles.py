d59060b6005c15b516a57e4b40f880376af409af
Paolo Fossati <paolo.fossati98@gmail.com>
2019-11-03 01:03:18 -0400

    yo

--- rotateTiles.py
+++ rotateTiles.py
@@ -9,6 +9,11 @@ import numpy as np
 import imutils
 import glob, os
 
+if os.path.exists('path\rot_tiles') == False:
+    os.makedirs('rot_tiles')
+else:
+    pass
+
 for i,file in enumerate(glob.glob("tiles/*.jpg")):
     image = cv2.imread(file) 
     height, width = image.shape[:2]
@@ -16,5 +21,5 @@ for i,file in enumerate(glob.glob("tiles/*.jpg")):
     angle = np.random.randint(1, 360)
     print(angle)
     rot_image = imutils.rotate_bound(image, angle)
-    cv2.imwrite(str(file)+str(i)+'rot.jpg', rot_image)
+    cv2.imwrite('path\rot_tiles\ ' str(file) + str(i) + 'rot' + str(angle) + '.jpg', rot_image)
 
